e431b3c20c5a8660eb54f7d45b2b04fbf8efb255
9aoyang <9aoyang@gmail.com>
2017-05-06 15:12:13 +0800

    media_error test

--- webvilletv/webvilletv.js
+++ webvilletv/webvilletv.js
@@ -5,37 +5,48 @@ var palylist;
 //video元素的引用
 var video;
 
-window.onload = function() {
+window.onload = function () {
     playlist = ["video/preroll",
-                "video/areyoupopular",
-                "video/destinationearth"];
+        "video/areyoupopular",
+        "video/destinationearth"
+    ];
     video = document.getElementById("video");
     video.addEventListener("ended", nextVideo, false);
     video.src = playlist[position] + getFormatExtension();
     video.load();
     video.play();
     //alert("Playing " + video.currentSrc);
+    video.addEventListener("error", errorHandler, false);
+
 }
 
 function nextVideo() {
-    
+
     position++;
     if (position >= video.length) {
         position = 0;
     }
     video.src = playlist[position] + getFormatExtension();
     video.load();
-    video.play(); 
+    video.play();
 }
 
 function getFormatExtension() {
-    if(video.canPlayType("video/mp4") != ""){
+    if (video.canPlayType("video/mp4") != "") {
         return ".mp4";
     }
-    if(video.canPlayType("video/webm") != ""){
+    if (video.canPlayType("video/webm") != "") {
         return ".webm";
     }
-    if(video.canPlayType("video/ogg") != ""){
+    if (video.canPlayType("video/ogg") != "") {
         return ".ogv"
+    }
+}
+
+function errorHandler() {
+    var video = document.getElementById("video");
+    if (video.error) {
+            video.poster = "images/technicaldifficulties.jpg";
+            alert(video.error.code);
     }
 }
