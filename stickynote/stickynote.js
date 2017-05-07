9ff986c729605594000f0a3ae84e41c3ddb7c82c
9aoyang <9aoyang@gmail.com>
2017-05-07 17:53:47 +0800

    add colorful function module

--- stickynote/stickynote.js
+++ stickynote/stickynote.js
@@ -8,35 +8,57 @@ function init() {
 
     for (var i = 0; i < stickiesArray.length; i++) {
         var key = stickiesArray[i];
-        var value = localStorage[key];
+        var value = JSON.parse(localStorage[key]);
         addStickyToDOM(key, value);
     }
 
 }
 
 function createSticky() {
-    var stickiesArray = getStickiesArray();
-    var currentDate = new Date();
-    var key = "sticky_" + currentDate.getTime();
-    var value = document.getElementById("note_text").value;
+	var stickiesArray = getStickiesArray();
+	var value = document.getElementById("note_text").value;
+	var colorSelectObj = document.getElementById("note_color");
+	var index = colorSelectObj.selectedIndex;
+	var color = colorSelectObj[index].value;
 
-    localStorage.setItem(key, value);
-    stickiesArray.push(key);
-    localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	// create sticky note using JSON to hold value and color
+	var currentDate = new Date();
+	var key = "sticky_" + currentDate.getTime();
+	var stickyObj = {
+			"value": value,
+			"color": color
+	};
+	localStorage.setItem(key, JSON.stringify(stickyObj));
 
-    addStickyToDOM(key, value);
+	// add new sticky note key to array and update notes array in localStorage
+	stickiesArray.push(key);
+	localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	
+	addStickyToDOM(key, stickyObj);
 }
 
-function addStickyToDOM(key, value) {
-    var stickies = document.getElementById("stickies");
-    var sticky = document.createElement("li");
-    sticky.setAttribute("id", key);
-    var span = document.createElement("span");
-    span.setAttribute("class", "sticky");
-    span.innerHTML = value;
-    sticky.appendChild(span);
-    stickies.appendChild(sticky);
-    sticky.onclick = deleteSticky;
+function addStickyToDOM(key, stickyObj) {
+	var stickies = document.getElementById("stickies");
+
+	var sticky = document.createElement("li");
+	// set the id attribute to the key so we can find it using
+	// the ids stored in the stickies array
+	sticky.setAttribute("id", key);
+	// use the stickyObj color, and set the background-color CSS style
+	sticky.style.backgroundColor = stickyObj.color;
+
+	var span = document.createElement("span");
+	span.setAttribute("class", "sticky");
+
+	// use the stickyObj value as the text for the sticky note 
+	span.innerHTML = stickyObj.value;
+
+	// add everything to the DOM
+	sticky.appendChild(span);
+	stickies.appendChild(sticky);
+
+	// add an event listener so when you click on a sticky note it can be deleted
+	sticky.onclick = deleteSticky;
 }
 
 function getStickiesArray() {
@@ -51,21 +73,21 @@ function getStickiesArray() {
 }
 
 function deleteSticky(e) {
-	var key = e.target.id;
-	if (e.target.tagName.toLowerCase() == "span") {
-		key = e.target.parentNode.id;
-	}
-	localStorage.removeItem(key);
-	var stickiesArray = getStickiesArray();
-	if (stickiesArray) {
-		for (var i = 0; i < stickiesArray.length; i++) {
-			if (key == stickiesArray[i]) {
-				stickiesArray.splice(i,1);
-			}
-		}
-		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
-		removeStickyFromDOM(key);
-	}
+    var key = e.target.id;
+    if (e.target.tagName.toLowerCase() == "span") {
+        key = e.target.parentNode.id;
+    }
+    localStorage.removeItem(key);
+    var stickiesArray = getStickiesArray();
+    if (stickiesArray) {
+        for (var i = 0; i < stickiesArray.length; i++) {
+            if (key == stickiesArray[i]) {
+                stickiesArray.splice(i, 1);
+            }
+        }
+        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+        removeStickyFromDOM(key);
+    }
 }
 
 function removeStickyFromDOM(key) {
