a48bd08ca8bc625297eca1df5a9a626c86b3e031
9aoyang <9aoyang@gmail.com>
2017-05-11 14:30:35 +0800

    format creamcandy

--- creamcandy/creamcandy.js
+++ creamcandy/creamcandy.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     setInterval(handleRefresh, 3000);
     /*
     var url = "http://localhost/creamcandy/sales.json";
@@ -13,40 +13,40 @@ window.onload = function() {
     */
 }
 
-function updateSales (sales) {
+function updateSales(sales) {
     var salesDiv = document.getElementById("sales");
     //salesDiv.innerHTML = responseText;
     //var sales = JSON.parse(responseText);
-    for (var i=0; i<sales.length; i++) {
+    for (var i = 0; i < sales.length; i++) {
         var sale = sales[i];
         var div = document.createElement("div");
         div.setAttribute("class", "saleItem");
         div.innerHTML = sale.name + " sold " + sale.sales + " Creamcandy";
         salesDiv.appendChild(div);
     }
-    if(sales.length > 0) {
+    if (sales.length > 0) {
         lastReportTime = sales[sales.length - 1].time;
     }
 }
 
 var lastReportTime = 0;
+
 function handleRefresh() {
     //alert("i'm alive");
-	var url = "http://gumball.wickedlysmart.com" +
-				"?callback=updateSales" +
-				"&lastreporttime=" + lastReportTime +
-				"&random=" + (new Date()).getTime();
+    var url = "http://gumball.wickedlysmart.com" +
+        "?callback=updateSales" +
+        "&lastreporttime=" + lastReportTime +
+        "&random=" + (new Date()).getTime();
 
     var newScriptElement = document.createElement("script");
     newScriptElement.setAttribute("src", url);
     newScriptElement.setAttribute("id", "jsonp");
     var oldScriptElement = document.getElementById("jsonp");
-    var head = document.getElementsByTagName("head")[0];  
-	if (oldScriptElement == null) {
- 
-		head.appendChild(newScriptElement);
-	}
-	else {
-		head.replaceChild(newScriptElement, oldScriptElement);
-	}
+    var head = document.getElementsByTagName("head")[0];
+    if (oldScriptElement == null) {
+
+        head.appendChild(newScriptElement);
+    } else {
+        head.replaceChild(newScriptElement, oldScriptElement);
+    }
 }
